bb3d282667c1cd78c9171e8f92ff89c2bb775bae
Sayan <bhattacharya.sayan30@gmail.com>
2023-01-05 01:07:45 +0530

    results

--- src/routes/answer/createAnswer.js
+++ src/routes/answer/createAnswer.js
@@ -1,15 +1,35 @@
 const db = require("../../firebase");
+const getSnapData = require("../../utils/getSnapData");
 
 const createAnswer = async (req, res) => {
-  console.log(req.body);
+  const exam_id = req.body[0]?.exam_id;
+  const student_id = req.body[0]?.student_id;
+
+  const snap = await db
+    .collection("question")
+    .where("exam_id", "==", exam_id)
+    .get();
+  const questions = getSnapData(snap);
+
   const batch = db.batch();
+  let marks = 0;
   for (const answer of req.body) {
     batch.set(db.collection("answer").doc(), answer);
+    for (const question of questions) {
+      if (question.answer === answer.mcq || question.answer === answer.answer) {
+        marks += +question.marks;
+      }
+    }
   }
 
   await batch.commit();
+  await db.collection("result").add({
+    student_id,
+    exam_id,
+    marks,
+  });
 
-  res.json({});
+  res.sendStatus(200);
 };
 
 module.exports = createAnswer;
