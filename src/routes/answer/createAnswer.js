dd3da0735f8c44d4a012287ad3cc55f261959b96
ayanNandi410 <ayan.nandi@quantzent.com>
2023-02-05 16:39:13 +0530

    Correction in marks calculation

--- src/routes/answer/createAnswer.js
+++ src/routes/answer/createAnswer.js
@@ -4,6 +4,7 @@ const getSnapData = require("../../utils/getSnapData");
 const createAnswer = async (req, res) => {
   const exam_id = req.body[0]?.exam_id;
   const student_id = req.body[0]?.student_id;
+  const timestamp = req.body[0]?.timestamp;
 
   const snapS = await db.collection("student").where("id", "==", student_id).get();
   dataS = getSnapData(snapS);
@@ -29,7 +30,7 @@ const createAnswer = async (req, res) => {
   for (const answer of req.body) {
     
     for (const question of questions) {
-      if (question.answer === answer.mcq || question.answer === answer.answer) {
+      if ((question.answer === answer.mcq) && (question.qid === answer.question_id)) {
         marks += +question.marks;
       }
     }
@@ -41,7 +42,8 @@ const createAnswer = async (req, res) => {
     student_name,
     exam_id,
     marks,
-    total
+    total,
+    timestamp
   });
 
   res.sendStatus(200);
