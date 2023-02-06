edf208f5ccd368458d663ccded28162ce5b9732d
Sayan <bhattacharya.sayan30@gmail.com>
2023-02-06 18:24:59 +0530

    config changes

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,6 +1,26 @@
 const firebase = require("firebase");
 require("firebase/firestore");
-const firebaseConfig = require("./config");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+} = process.env;
+
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+};
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
