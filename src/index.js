6683780da53106e970a62b585f1af238c8d23bd5
Sayan <bhattacharya.sayan30@gmail.com>
2023-02-07 22:07:39 +0530

    make public folder

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const RootRouter = require("./routes");
 const path = require("path");
+const fs = require("fs");
+
+console.log(__dirname, "../public");
+if (!fs.existsSync(path.join(__dirname, "../public")))
+  fs.mkdirSync(path.join(__dirname, "../public"));
 
 const app = express();
 
